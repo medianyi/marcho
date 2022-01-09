491b4ec8f54d26b24369b62f58f4a3ba1fcc1cb5
medianyi <vladimirmedianyi@gmail.com>
2022-01-09 22:44:47 +0200

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,57 @@
-$(function () {
+$(function(){
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item--active').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+
+  })
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  })
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  })
+
+  $('.select-style, .product-one__item-num').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
   
   $('.top-slider__inner').slick({
     dots:true,
@@ -6,7 +59,7 @@ $(function () {
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000,
-  })
+  });
   
   $(".star").rateYo({
     starWidth: "17px",
@@ -58,5 +111,6 @@ $(function () {
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
 
+  const { on } = require("gulp");
 
 });
