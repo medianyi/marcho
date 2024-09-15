1b37e7cdbe99ae13b9ac7521d2f8e09a0b638694
medianyi <vladimirmedianyi@gmail.com>
2024-09-15 19:00:22 +0300

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-  $('body').hide();
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true, 
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
 });
--- gulpfile.mjs
+++ gulpfile.mjs
@@ -27,6 +27,8 @@ async function styles() {
 
 function scripts() {
   return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
